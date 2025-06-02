6e1ea7db2fa5a6e9a2d3faaa597352c6f70418a7
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2025-06-02 09:53:01 -0300

    criando conexão com o banco

---
+++ db.js
@@ -0,0 +1,15 @@
+// db.js
+const mysql = require('mysql2/promise');
+
+// Cria a conexão com o banco de dados
+const db = mysql.createPool({
+  host: 'localhost',       // ou IP do banco
+  user: 'root',     // ex: 'root'
+  password: 'fatec',   // ex: '1234'
+  database: 'portifolio', // substitua pelo nome real do banco
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = db;
